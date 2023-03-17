6aceb5e1247d4bc9fae8912077b3d1733a9717f2
Rajan Phadnis <22627030+rajanphadnis@users.noreply.github.com>
2023-03-17 04:34:13 -0400

    testing update mechanism again

--- main/public/service-worker.js
+++ main/public/service-worker.js
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var dataCacheName = "v1.1.2";
-var cacheName = "PWA-v1.1.2";
+var dataCacheName = "v1.1.3";
+var cacheName = "PWA-v1.1.3";
 var filesToCache = [
   new Request("/index.html", { cache: "no-cache" }),
   new Request("/scripts/calendar.js", { cache: "no-cache" }),
